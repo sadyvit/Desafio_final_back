dedd5d84b40a82e09b48468c0b09dae1694cf9c5
sadyvit <sadyvitor@hotmail.com>
2026-03-03 01:49:32 -0300

    feat(app): new vercel postgres connection string

--- src/bancodedados/conexao.js
+++ src/bancodedados/conexao.js
@@ -1,7 +1,12 @@
+const connectionString =
+  process.env.DATABASE_URL ||
+  process.env.POSTGRES_URL ||
+  process.env.POSTGRES_URL_NON_POOLING;
+
 const knex = require("knex")({
   client: "pg",
   connection: {
-    connectionString: process.env.DATABASE_URL,
+    connectionString,
     ssl: { rejectUnauthorized: false },
   },
   pool: { min: 0, max: 10 },
